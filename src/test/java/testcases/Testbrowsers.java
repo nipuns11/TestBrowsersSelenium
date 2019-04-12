aee2f22c075d88c70c278dd2073ee799cb04e946
nipuns11 <nipunsh19@gmail.com>
2019-04-11 20:19:17 -0400

    coments added for tests

--- src/test/java/testcases/Testbrowsers.java
+++ src/test/java/testcases/Testbrowsers.java
@@ -1,13 +1,19 @@
 package testcases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Testbrowsers {
-	
+public class Testbrowsers  {
+
+	public static String browser = "chrome";
+	public static WebDriver driver;// web driver interface to reuse driver interface for different browsers 
+
 	public static void main(String[] args)
 	{
 		//latest selenium jar file 
@@ -17,16 +23,38 @@ public class Testbrowsers {
 		// dependency tool and build tool - Maven 
 		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\NipunSharma\\Downloads\\automation\\seleniumjar\\geckodriver.exe");
 		// updated pom xml with webdriver manager no need to download executables 
-		WebDriverManager.firefoxdriver().setup();
 		
-		FirefoxDriver driver = new FirefoxDriver();
-		driver.get("http://nippun.blogspot.com/");
-		String title = driver.getTitle();
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			//webdriver interface https://seleniumhq.github.io/selenium/docs/api/java/
+			driver = new ChromeDriver();
+			
+			
+		}else if(browser.equals("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			//webdriver interface https://seleniumhq.github.io/selenium/docs/api/java/
+			driver = new FirefoxDriver();
+			
+		}
 		
-		driver.close();
-		
-		
+		else if(browser.equals("ie")) {
+		WebDriverManager.iedriver().setup();
+		//webdriver interface https://seleniumhq.github.io/selenium/docs/api/java/
+		driver = new InternetExplorerDriver();
 	}
 	
+		driver.get("http://nippun.blogspot.com/");
+		
+		driver.findElement(By.cssSelector("div.date-outer:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(5) > a:nth-child(1)")).click();;
+		
+		
+		WebElement Lifetitle = driver.findElement(By.cssSelector("h1.title"));// importance of webelement in maniuplation 
+		Lifetitle.click();
+		
+		driver.close();//current browser windows 
+		driver.quit();//current browser windows plus all realted browsers 
+		
+	}
 
 }
